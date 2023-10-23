144af312152bc8d91b3b611bb78b0d7a2065c7e8
aajafry <aajafry2@gmail.com>
2023-10-23 14:24:18 +0600

    fix the re-rendering issue.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
  // Import the functions we need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getDatabase, onValue, ref, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-     // Firebase configuration
+
+// Firebase configuration
  const firebaseConfig = {
  apiKey: "AIzaSyCIBeznpoOXpU_tJ2Eb6lc0UNwlxM-aGZ8",
  authDomain: "spondonblooddonationdb.firebaseapp.com",
@@ -10,13 +11,10 @@ import { getDatabase, onValue, ref, set } from "https://www.gstatic.com/firebase
  messagingSenderId: "916618952855",
  appId: "1:916618952855:web:0e9b9c6e39b5a5beb5a9d2"
  };
- 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
- 
  //connect to db of firebase.
  const db = getDatabase(app);
- 
  // select the DOM elements for doner registration.
  const donerName = document.querySelector("#rgName");
  const donerAge = document.querySelector("#rgAge");
@@ -35,73 +33,84 @@ import { getDatabase, onValue, ref, set } from "https://www.gstatic.com/firebase
  const filterBloodGroup = document.querySelector("#filter__blood_group");
  const filterLocation = document.querySelector("#filter__location");
  const filterBtn = document.querySelector(".filter_btn");
-
- // variable declaretion.
- let UID = 0;
+ 
+// variable declaretion.
+let UID = 0;    // for unique identity of donor data set.
+let SID = 0;    // for the serial issue. 
+//  template of table row.
+const tableRowHeaderTemp = `<tr>
+    <th>SL</th>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Gender</th>
+    <th>Religion</th>
+    <th>Blood Group</th>
+    <th>Phone</th>
+    <th>Address</th>
+</tr>`
  // event declaretion for doner registration and filter.
  // all the select box change event on fire description.
  const changeEvents = async (thisElement) => {
-     let changeValue = await thisElement.value;
-     return changeValue;
- }
-        
+    let changeValue = await thisElement.value;
+    return changeValue;
+ }        
  // db write code. 
  submitBtn.addEventListener("click", function (event) {        
-     event.preventDefault();
-     set(ref(db, "user/" + UID++),
+    event.preventDefault(); 
+    set(ref(db, "user/" + UID++),
      {
-         serial: UID,
-         donerName: donerName.value,
-         donerAge: donerAge.value,
-         donerGender: donerGender.value,
-         donerReligion: donerReligion.value,
-         donerBloodGroup: donerBloodGroup.value,
-         donerPhone: donerPhone.value,
-         donerLocation: donerLocation.value
-     })
-     alert("Thank You be a Doner!");
+        donerName: donerName.value,
+        donerAge: donerAge.value,
+        donerGender: donerGender.value,
+        donerReligion: donerReligion.value,
+        donerBloodGroup: donerBloodGroup.value,
+        donerPhone: donerPhone.value,
+        donerLocation: donerLocation.value
+    })
+    alert("Thank You be a Doner!");
  })
-
  // db read code.
  const userRef = ref(db, "user/");
      
  onValue(userRef, (snapshot) => {
-     const userSnapshots = snapshot.val();
-     userSnapshots.map(data => {
-         let template = `<tr>
-                 <td>${data.serial}</td>
-                 <td>${data.donerName}</td>
-                 <td>${data.donerAge}</td>
-                 <td>${data.donerGender}</td>
-                 <td>${data.donerReligion}</td>
-                 <td>${data.donerBloodGroup}</td>
-                 <td>${data.donerPhone}</td>
-                 <td>${data.donerLocation}</td> 
-             </tr>`
-             donerInfotable.innerHTML += template;
-         });
-     // show filtered doner information.
-     // add event listener for filtered doner.
-     filterBtn.addEventListener("click", function(event){
-         event.preventDefault();
-         donerInfotable.innerHTML = "";
-         userSnapshots.filter((data) => {
-             if(data.donerGender == filterGender.value || data.donerReligion == filterReligion.value || data.donerBloodGroup == filterBloodGroup.value || data.donerLocation == filterLocation.value){
-                 let template = `<tr>
-                     <td>${data.serial}</td>
-                     <td>${data.donerName}</td>
-                     <td>${data.donerAge}</td>
-                     <td>${data.donerGender}</td>
-                     <td>${data.donerReligion}</td>
-                     <td>${data.donerBloodGroup}</td>
-                     <td>${data.donerPhone}</td>
-                     <td>${data.donerLocation}</td> 
-                 </tr>`
-                 donerInfotable.innerHTML += template;
-             }
-             else {
-                 return donerInfotable;
-             }  
+    const userSnapshots = snapshot.val();
+    SID = 1;
+    donerInfotable.innerHTML = tableRowHeaderTemp;
+    userSnapshots?.map(data => {
+        let template = `<tr>
+            <td>${SID++}</td>
+            <td>${data.donerName}</td>
+            <td>${data.donerAge}</td>
+            <td>${data.donerGender}</td>
+            <td>${data.donerReligion}</td>
+            <td>${data.donerBloodGroup}</td>
+            <td>${data.donerPhone}</td>
+            <td>${data.donerLocation}</td> 
+        </tr>`
+        donerInfotable.innerHTML += template;
+    });
+    // show filtered doner information.
+    // add event listener for filtered doner.
+    filterBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        SID = 1;
+        donerInfotable.innerHTML = tableRowHeaderTemp;
+        userSnapshots?.filter((data) => {
+            if(data.donerGender == filterGender.value || data.donerReligion == filterReligion.value || data.donerBloodGroup == filterBloodGroup.value || data.donerLocation == filterLocation.value){
+                let template = `<tr>
+                    <td>${SID++}</td>
+                    <td>${data.donerName}</td>
+                    <td>${data.donerAge}</td>
+                    <td>${data.donerGender}</td>
+                    <td>${data.donerReligion}</td>
+                    <td>${data.donerBloodGroup}</td>
+                    <td>${data.donerPhone}</td>
+                    <td>${data.donerLocation}</td> 
+                </tr>`
+                donerInfotable.innerHTML += template;
+            }else {
+                return donerInfotable;
+            }  
          })
      })
  });
